cd3a7bde723fe7be0ba473f28d5f20790bccc40a
Palak <palakk.bais@gmail.com>
2025-07-26 11:44:31 +0530

    Initial commit

---
+++ chatbot-backend/chatbot-backend/src/main/java/com/ecommerce/chatbot/ChatbotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotBackendApplication.class, args);
+	}
+
+}
---
+++ chatbot-backend/chatbot-backend/src/test/java/com/ecommerce/chatbot/ChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
